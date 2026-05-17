06b27ea197d0562ec2909e2dc2f22733c5152f46
ARmin <armin1234573karimi@gmail.com>
2026-05-17 19:48:12 +0330

    update bot

--- bot.py
+++ bot.py
@@ -1353,13 +1353,18 @@ def main():
     app.add_handler(CallbackQueryHandler(admin_unblock, pattern="^admin_unblock$"))
     app.add_handler(CallbackQueryHandler(cancel_add_config, pattern="^cancel_add_config$"))
     
-    #admin_filter = filters.User(user_id=ADMINS)
 
-    #app.add_handler(
-        #MessageHandler(admin_filter & filters.TEXT & ~filters.COMMAND, admin_message_handler))
+    admin_filter = filters.User(user_id=ADMINS)
 
     app.add_handler(
-        MessageHandler(filters.TEXT & ~filters.COMMAND, menu_message_handler))
+        MessageHandler(admin_filter & filters.TEXT & ~filters.COMMAND, admin_message_handler),
+        block=False
+    )
+
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, menu_message_handler)
+    )
+
 
     print("BOT STARTED")
 
